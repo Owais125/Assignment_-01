15adbc67ff32a6f69f9439021058dcae5af219df
Owais_khatri <owaisraza12368@gmail.com>
2023-09-11 12:33:56 -0700

    Work Update

---
+++ App.js
@@ -0,0 +1,160 @@
+// Chapter 01
+
+
+// Q:1
+
+// Q:1 sub
+alert("Owais");
+// Q:2sub
+alert("Raza");
+// Q:3sub
+alert("abc12368@gmail.com");
+// Q:4sub
+alert("03124569855");
+// Q:5sub
+alert("1234567");
+
+// Q:2
+alert("You're learning JavaScript!");
+
+// Q:3
+alert("Welcome To my Page");
+
+// Chapter 01 End
+
+
+// Chapter 02
+
+// Q:01
+var firstName = 'Hello' ;
+
+alert(firstName);
+
+// Q:2
+var first=
+
+first = 'Pakistan';
+
+alert(first);
+
+// Q:3
+var Double_A = 'Welocme to My Team Double_A Web Developer';
+
+alert(Double_A);
+
+// Q:4
+var bestMan = "Charlie";
+
+bestMan =  "johns"
+
+alert(bestMan);
+
+// Chapter 02 End
+
+// Chapter 03
+
+// Q:1
+var caseQty=144;
+// Q:2
+var caseQty;
+
+alert(caseQty);
+
+// Q:3
+
+var num1=5;
+var num2=4;
+
+console.log(num1+num2);
+
+// Q:4
+var num01=15;
+var num02=45;
+
+console.log(num01+num02);
+
+// Q:5
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+                 //    100  +       10    = 110
+
+                 
+// Q:6
+var one = 5
+var two = 8
+
+var res = one + two;
+
+console.log(res)
+
+
+// Chapter 03 End
+
+// Chapter 04
+
+// Q:1
+var productcost = 3.45;
+console.log(productcost);
+
+
+// Q:2
+var NameOfBand = 'Jabbar';
+
+console.log(NameOfBand);
+
+
+// Q:3
+var name = "khatri";
+document.write(name);
+
+
+// Q:4
+var firstName = 'Owais';
+var lastName = 'Raza';
+var fullName = firstName +  lastName;
+
+console.log(fullName);
+
+// Q:5
+// Legal Variable = any name , number .
+// iLegal Variable = any special character ex:%@#^&*-+-/
+
+// Chapter 04 End
+
+// Chapter 5
+
+// Q:1
+// Answer:Modules
+
+// Q:2
+var num001 = 20 % 6;
+console.log(num001);
+
+// Q:3
+var numone=0.5;
+var numtwo = 2000;
+
+var result= numone * numtwo
+
+console.log(result);
+
+// Q:4
+var subone = 45;
+var subtwo = 15;
+
+var subres = subone - subtwo;
+
+console.log(subres);
+
+// Q:5
+
+var numberone = 15 %  7;
+
+console.log(numberone);
+
+// Q:6
+var numbertwo = 5*4;
+
+alert(numbertwo);
